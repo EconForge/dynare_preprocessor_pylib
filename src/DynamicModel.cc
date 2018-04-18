d686275da142e2d1e1afcdb32785c25f25b5a5a7
Houtan Bastani <houtan@dynare.org>
2018-04-18 14:29:40 +0200

    fix bug with predetermined variables used in model local variables

--- src/DynamicModel.cc
+++ src/DynamicModel.cc
@@ -5250,6 +5250,10 @@ DynamicModel::substituteExpectation(bool partial_information_model)
 void
 DynamicModel::transformPredeterminedVariables()
 {
+  for (map<int, expr_t>::iterator it = local_variables_table.begin();
+       it != local_variables_table.end(); it++)
+    it->second = it->second->decreaseLeadsLagsPredeterminedVariables();
+
   for (int i = 0; i < (int) equations.size(); i++)
     {
       BinaryOpNode *substeq = dynamic_cast<BinaryOpNode *>(equations[i]->decreaseLeadsLagsPredeterminedVariables());
