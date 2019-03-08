EconForge/dynare_preprocessor_pylib

commit 7d16fff786c477c7ff6ddf8a52a0fa61b670eb50

Further simplifications

src/ModelTree.cc
@@ -411,7 +411,7 @@ ModelTree::evaluateAndReduceJacobian(const eval_context_t &eval_context, jacob_m
             {
               if (verbose)
                 cout << "the coefficient related to variable " << var << " with lag " << lag << " in equation " << eq << " is equal to " << val << " and is set to 0 in the incidence matrix (size=" << symbol_table.endo_nbr() << ")" << endl;
-              jacobian_elements_to_delete.insert({ eq, deriv_id });
+              jacobian_elements_to_delete.emplace(eq, deriv_id);
             }
           else
             {

src/DynamicModel.cc
@@ -57,7 +57,7 @@ DynamicModel::copyHelper(const DynamicModel &m)
     first_chain_rule_derivatives[it.first] = f(it.second);
 
   for (const auto &it : m.equation_type_and_normalized_equation)
-    equation_type_and_normalized_equation.push_back({it.first, f(it.second)});
+    equation_type_and_normalized_equation.emplace_back(it.first, f(it.second));
 
   for (const auto &it : m.blocks_derivatives)
     {
@@ -4464,7 +4464,7 @@ DynamicModel::getPacMaxLag(const string &pac_model_name, map<pair<string, string
                 break;
               }
         string eq = eqtag_and_lag[{pac_model_name, eqtag}].first;
-        eqtag_and_lag[{pac_model_name, eqtag}] = {eq, equation->PacMaxLag((*(endogs.begin())).first)};
+        eqtag_and_lag[{pac_model_name, eqtag}] = {eq, equation->PacMaxLag(endogs.begin()->first)};
       }
 }
 

src/ModelEquationBlock.cc
@@ -36,7 +36,7 @@ SteadyStateModel::SteadyStateModel(const SteadyStateModel &m) :
   static_model {m.static_model}
 {
   for (const auto &it : m.def_table)
-    def_table.push_back({it.first, it.second->clone(*this)});
+    def_table.emplace_back(it.first, it.second->clone(*this));
 }
 
 SteadyStateModel &
@@ -48,7 +48,7 @@ SteadyStateModel::operator=(const SteadyStateModel &m)
 
   def_table.clear();
   for (const auto &it : m.def_table)
-    def_table.push_back({it.first, it.second->clone(*this)});
+    def_table.emplace_back(it.first, it.second->clone(*this));
 
   return *this;
 }
@@ -298,7 +298,7 @@ Epilogue::Epilogue(const Epilogue &m) :
   DynamicModel {m}
 {
   for (const auto &it : m.def_table)
-    def_table.push_back({it.first, it.second->clone(*this)});
+    def_table.emplace_back(it.first, it.second->clone(*this));
 }
 
 Epilogue &
@@ -308,7 +308,7 @@ Epilogue::operator=(const Epilogue &m)
 
   def_table.clear();
   for (const auto &it : m.def_table)
-    def_table.push_back({it.first, it.second->clone(*this)});
+    def_table.emplace_back(it.first, it.second->clone(*this));
 
   return *this;
 }

src/StaticModel.cc
@@ -55,7 +55,7 @@ StaticModel::copyHelper(const StaticModel &m)
     first_chain_rule_derivatives[it.first] = f(it.second);
 
   for (const auto &it : m.equation_type_and_normalized_equation)
-    equation_type_and_normalized_equation.push_back({it.first, f(it.second)});
+    equation_type_and_normalized_equation.emplace_back(it.first, f(it.second));
 
   for (const auto &it : m.blocks_derivatives)
     {

commit d9f7ac4c9be88f1e6d8b953c0c71195e1fa31f8c

Fix bug introduced in 7d16fff786c477c7ff6ddf8a52a0fa61b670eb50
Here, emplace() and insert() are not interchangeable because this is a std::set of
std::vector<int>, and we really want the initializer-list constructor of
std::vector (and not the two int's constructor).

Fixes the test deterministic_simulations/lola_solve_one_boundary.mod

src/ModelTree.cc
@@ -411,7 +411,7 @@ ModelTree::evaluateAndReduceJacobian(const eval_context_t &eval_context, jacob_m
             {
               if (verbose)
                 cout << "the coefficient related to variable " << var << " with lag " << lag << " in equation " << eq << " is equal to " << val << " and is set to 0 in the incidence matrix (size=" << symbol_table.endo_nbr() << ")" << endl;
-              jacobian_elements_to_delete.emplace(eq, deriv_id);
+              jacobian_elements_to_delete.insert({ eq, deriv_id });
             }
           else
             {