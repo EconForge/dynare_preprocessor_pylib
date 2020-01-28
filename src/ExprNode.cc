EconForge/dynare_preprocessor_pylib

commit 985d74238170a06509c7239ba1df48b0d645ff6f

Removed prefixing of doubles between -1 and 1 with 0.
This resulted in values like `00.018` in the AST's NumConstNode which
cannot be read by some JSON readers (e.g. Mathworks' jsondecode).

src/ExprNode.cc
@@ -451,8 +451,6 @@ void
 NumConstNode::writeJsonAST(ostream &output) const
 {
   output << R"({"node_type" : "NumConstNode", "value" : )";
-  if (double testval = datatree.num_constants.getDouble(id); testval < 1.0 && testval > -1.0 && testval != 0.0)
-    output << "0";
   output << datatree.num_constants.get(id) << "}";
 }
 

commit 2479a2a78c2450be092cd11581dd14df95bc6415

Fixed bug introduced in 985d742.
If a float smaller than one in absolute value is written without a
leading zero in the mod file, for instance as `.5`, we really need to
prefix the number with a zero. The simplest approach is to convert
the strings representing the numerical constants into floats.

src/ExprNode.cc
@@ -451,7 +451,7 @@ void
 NumConstNode::writeJsonAST(ostream &output) const
 {
   output << R"({"node_type" : "NumConstNode", "value" : )";
-  output << datatree.num_constants.get(id) << "}";
+  output << std::stof(datatree.num_constants.get(id)) << "}";
 }
 
 void