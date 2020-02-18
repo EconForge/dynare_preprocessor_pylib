EconForge/dynare_preprocessor_pylib

commit a778ba4ab4c49669f8a59ef4aa7176063b590f1e

fix lookbehind to handle `-` sign in substitution of dates in native statements
closes dynare#1708

src/Statement.cc
@@ -54,8 +54,8 @@ NativeStatement::writeOutput(ostream &output, const string &basename, bool minim
 {
   using namespace boost::xpressive;
   string date_regex = R"((-?\d+([YyAa]|[Mm]([1-9]|1[0-2])|[Qq][1-4])))";
-  sregex regex_lookbehind = sregex::compile(R"((?<!\$|\d|[a-zA-Z_]|\'))" + date_regex);
-  sregex regex_dollar = sregex::compile(R"((\$))"+date_regex);
+  sregex regex_lookbehind = sregex::compile(R"((?<!\$|[a-zA-Z_]|-|'))" + date_regex);
+  sregex regex_dollar = sregex::compile(R"((\$))" + date_regex);
 
   string ns = regex_replace(native_statement, regex_lookbehind, "dates('$&')");
   ns = regex_replace(ns, regex_dollar, "$2"); //replace $DATE with DATE

commit a3fb2b123ead324f25bd9ca2231a1cc628531873

fix bug introduced in a778ba4

src/Statement.cc
@@ -54,7 +54,7 @@ NativeStatement::writeOutput(ostream &output, const string &basename, bool minim
 {
   using namespace boost::xpressive;
   string date_regex = R"((-?\d+([YyAa]|[Mm]([1-9]|1[0-2])|[Qq][1-4])))";
-  sregex regex_lookbehind = sregex::compile(R"((?<!\$|[a-zA-Z_]|-|'))" + date_regex);
+  sregex regex_lookbehind = sregex::compile(R"((?<!\$|\d|[a-zA-Z_]|-|'))" + date_regex);
   sregex regex_dollar = sregex::compile(R"((\$))" + date_regex);
 
   string ns = regex_replace(native_statement, regex_lookbehind, "dates('$&')");