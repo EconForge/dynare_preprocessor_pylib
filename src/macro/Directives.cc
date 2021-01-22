EconForge/dynare_preprocessor_pylib

commit 4c14dc3fbf17bed9cfd66a8d86b5eb57aba39a6e

Macro processor: gracefully fail when argument to @#ifdef/@#ifndef is not a variable name
Ref. dynare#1771

src/macro/Directives.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2019-2020 Dynare Team
+ * Copyright © 2019-2021 Dynare Team
  *
  * This file is part of Dynare.
  *
@@ -253,7 +253,9 @@ If::interpret(ostream &output, Environment &env, vector<filesystem::path> &paths
           {
             first_clause = false;
             VariablePtr vp = dynamic_pointer_cast<Variable>(expr);
-            assert(vp);
+            if (!vp)
+              error(StackTrace(ifdef ? "@#ifdef" : "@ifndef",
+                               "The condition must be a variable name", location));
             if ((ifdef && env.isVariableDefined(vp->getName()))
                 || (ifndef && !env.isVariableDefined(vp->getName())))
               {

commit 1e5de7d71e78b7151c13190fb1de69340afc6844

Fix typo
Introduced in 4c14dc3fbf17bed9cfd66a8d86b5eb57aba39a6e.

src/macro/Directives.cc
@@ -254,7 +254,7 @@ If::interpret(ostream &output, Environment &env, vector<filesystem::path> &paths
             first_clause = false;
             VariablePtr vp = dynamic_pointer_cast<Variable>(expr);
             if (!vp)
-              error(StackTrace(ifdef ? "@#ifdef" : "@ifndef",
+              error(StackTrace(ifdef ? "@#ifdef" : "@#ifndef",
                                "The condition must be a variable name", location));
             if ((ifdef && env.isVariableDefined(vp->getName()))
                 || (ifndef && !env.isVariableDefined(vp->getName())))