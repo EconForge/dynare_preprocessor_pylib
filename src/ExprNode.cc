EconForge/dynare_preprocessor_pylib

commit d3e115194b551f318c0c036b5a115086c0891dd8

Fix output write_latex_original_model for model-local variables
Those variables were substituted out by early substitution methods (which are
also called on the original model, for an unknown reason).

Closes: #65

src/ExprNode.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2007-2020 Dynare Team
+ * Copyright © 2007-2021 Dynare Team
  *
  * This file is part of Dynare.
  *
@@ -1541,9 +1541,8 @@ VariableNode::VarMaxLag(const set<expr_t> &lhs_lag_equiv) const
 expr_t
 VariableNode::substituteAdl() const
 {
-  if (get_type() == SymbolType::modelLocalVariable)
-    return datatree.getLocalVariable(symb_id)->substituteAdl();
-
+  /* Do not recurse into model-local variables definition, rather do it at the
+     DynamicModel method level (see the comment there) */
   return const_cast<VariableNode *>(this);
 }
 
@@ -1988,9 +1987,8 @@ VariableNode::getEndosAndMaxLags(map<string, int> &model_endos_and_lags) const
 expr_t
 VariableNode::replaceVarsInEquation(map<VariableNode *, NumConstNode *> &table) const
 {
-  if (get_type() == SymbolType::modelLocalVariable)
-    return datatree.getLocalVariable(symb_id)->replaceVarsInEquation(table);
-
+  /* Do not recurse into model-local variables definition, because otherwise
+     this would substitute MLV in the original model (see #65). */
   for (auto &it : table)
     if (it.first->symb_id == symb_id)
       return it.second;

src/DynamicModel.cc
@@ -5579,6 +5579,13 @@ DynamicModel::substituteLeadLagInternal(AuxVarType type, bool deterministic_mode
 void
 DynamicModel::substituteAdl()
 {
+  /* Contrary to other substitution methods, we do the substitution in MLV
+     definitions here, instead of doing it at the ExprNode method level,
+     because otherwise this would substitute MLV in the original model (see
+     #65). */
+  for (auto &[id, definition] : local_variables_table)
+    definition = definition->substituteAdl();
+
   for (auto &equation : equations)
     equation = dynamic_cast<BinaryOpNode *>(equation->substituteAdl());
 }

commit 825b9ee8ef6e09860e2495dca944df28fc8c1688

Fix bugs introduced in d3e115194b551f318c0c036b5a115086c0891dd8
Ref. #65

src/ExprNode.cc
@@ -1636,9 +1636,9 @@ VariableNode::decreaseLeadsLags(int n) const
 expr_t
 VariableNode::decreaseLeadsLagsPredeterminedVariables() const
 {
-  if (get_type() == SymbolType::modelLocalVariable)
-    return datatree.getLocalVariable(symb_id)->decreaseLeadsLagsPredeterminedVariables();
-
+  /* Do not recurse into model-local variables definitions, since MLVs are
+     already handled by DynamicModel::transformPredeterminedVariables().
+     This is also necessary because of #65. */
   if (datatree.symbol_table.isPredetermined(symb_id))
     return decreaseLeadsLags(1);
   else
@@ -1987,8 +1987,9 @@ VariableNode::getEndosAndMaxLags(map<string, int> &model_endos_and_lags) const
 expr_t
 VariableNode::replaceVarsInEquation(map<VariableNode *, NumConstNode *> &table) const
 {
-  /* Do not recurse into model-local variables definition, because otherwise
-     this would substitute MLV in the original model (see #65). */
+  /* Do not recurse into model-local variables definitions, since MLVs are
+     already handled by ModelTree::simplifyEquations().
+     This is also necessary because of #65. */
   for (auto &it : table)
     if (it.first->symb_id == symb_id)
       return it.second;

src/ModelTree.cc
@@ -1639,6 +1639,8 @@ ModelTree::simplifyEquations()
   while (subst_table.size() != last_subst_table_size)
     {
       last_subst_table_size = subst_table.size();
+      for (auto &[id, definition] : local_variables_table)
+        definition = definition->replaceVarsInEquation(subst_table);
       for (auto &equation : equations)
         equation = dynamic_cast<BinaryOpNode *>(equation->replaceVarsInEquation(subst_table));
       subst_table.clear();