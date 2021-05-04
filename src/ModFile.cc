1d188a7444e9523c92bc1997783be8cc8800b30c
Sébastien Villemot <sebastien@dynare.org>
2021-05-04 17:21:47 +0200

    Add comment
    [skip ci]

--- src/ModFile.cc
+++ src/ModFile.cc
@@ -359,6 +359,7 @@ ModFile::checkPass(bool nostrict, bool stochastic)
         }
     }
 
+  // See dynare#1726
   if ((stochastic_statement_present || mod_file_struct.check_present) && dynamic_model.mfs > 0)
     {
       cerr << "ERROR: mfs > 0 is incompatible with check, stoch_simul, estimation, osr, ramsey_policy, discretionary_policy, calib_smoother, identification, methods_of_moments and sensitivity commands" << endl;
