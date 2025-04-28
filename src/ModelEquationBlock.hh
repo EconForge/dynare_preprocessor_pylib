06c8bd92ac8bb74a1a59b3e027fa48bdd5b77270
Sébastien Villemot <sebastien@dynare.org>
2025-04-28 16:50:06 +0200

    🐛 Fix linearity check for Ramsey models with lead/lag>1 on endos or >0 on exos
    Ref. dynare#1960
    
    (manually cherry picked from commit 7a758f6779e9665d1d0bdb5fb7191af3cccbc5cb)

--- src/ModelEquationBlock.hh
+++ src/ModelEquationBlock.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2010-2023 Dynare Team
+ * Copyright © 2010-2025 Dynare Team
  *
  * This file is part of Dynare.
  *
@@ -57,6 +57,17 @@ protected:
   modelClassName() const override
   {
     return "original Ramsey model";
+  }
+  int
+  getJacobianCol(int deriv_id, [[maybe_unused]] bool sparse) const override
+  {
+    /* Override the DynamicModel method by returning a dummy Jacobian column number.
+       The override is necessary because the method from DynamicModel fails with
+       endos with lag/lead greater than 1 or exos with a lag/lead, while substitutions
+       are by definition not done for an original model.
+       In particular, this fixes dynare#1960 (equation derivatives are computed for models declared
+       as linear, to check whether they are truly linear). */
+    return deriv_id;
   }
 };
 
